8ec839e9e26a2816171f1beffdf7ee81c0433acf
breno-assis9 <143261968+breno-assis9@users.noreply.github.com>
2023-12-22 19:29:52 -0300

    Implementado contexto de identidade.

---
+++ sgc/src/2.Services/Identity/sgc.Identity.Api/Configurations/IdentityConfig.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using sgc.Identity.Api.Data;
+using sgc.Core.Configuration.Identity;
+using sgc.Identity.Api.Extensions;
+
+namespace sgc.Identity.Api.Configuration
+{
+    public static class IdentityConfig
+    {
+        public static IServiceCollection AddIdentityConfiguration(this IServiceCollection services,
+           IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
+                .AddErrorDescriber<IdentityMenssagesPortuguese>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddJwtConfiguration(configuration);
+
+            return services;
+        }
+    }
+}
